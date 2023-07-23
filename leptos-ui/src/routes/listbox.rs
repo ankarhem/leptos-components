e799e789f1886e5a9b7d0d088bd46f47016ac18d
Jakob Ankarhem <jakob.ankarhem@outlook.com>
2023-07-23 14:56:05 +0200

    add checkmark icon

--- leptos-ui/src/routes/listbox.rs
+++ leptos-ui/src/routes/listbox.rs
@@ -2,13 +2,6 @@ use leptos::*;
 use leptos_a11y::listbox::*;
 
 #[component]
-fn DropdownIcon(cx: Scope) -> impl IntoView {
-    view! { cx,
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="h-5 w-5 text-gray-400"><path fill-rule="evenodd" d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z" clip-rule="evenodd"></path></svg>
-    }
-}
-
-#[component]
 pub fn ListboxPage(cx: Scope) -> impl IntoView {
     let options = vec!["one", "two"];
     let value = create_rw_signal(cx, options[0]);
@@ -38,7 +31,7 @@ pub fn ListboxPage(cx: Scope) -> impl IntoView {
                                     view! { cx,
                                         <ListboxOption value=option class="group flex cursor-default select-none items-center gap-3 py-2 px-4 focus:outline-none data-[active=true]:text-fuchsia-900 data-[active=true]:bg-fuchsia-100 aria-[selected=true]:font-semibold">
                                             <span class="w-5 group-aria-[selected=true]:visible invisible">
-                                                "✅"
+                                                <CheckmarkIcon />
                                             </span>
                                             {option}
                                         </ListboxOption>
@@ -50,5 +43,19 @@ pub fn ListboxPage(cx: Scope) -> impl IntoView {
                 </div>
             </div>
         </section>
+    }
+}
+
+#[component]
+fn CheckmarkIcon(cx: Scope) -> impl IntoView {
+    view! { cx,
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="h-5 w-5"><path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd"></path></svg>
+    }
+}
+
+#[component]
+fn DropdownIcon(cx: Scope) -> impl IntoView {
+    view! { cx,
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="h-5 w-5 text-gray-400"><path fill-rule="evenodd" d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z" clip-rule="evenodd"></path></svg>
     }
 }
